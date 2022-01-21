b3a933a4789709e7228bf39203b13dcaa86ab81f
dtlancaster <Tuscarora007@icloud.com>
2022-01-21 14:41:44 -0500

    upload 1

---
+++ easy/1. Two Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for index, value in enumerate(nums):
+            if target - value in dict:
+                return [dict[target - value], index]
+            else:
+                dict[value] = index
